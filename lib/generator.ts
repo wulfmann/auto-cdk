f837cfe17bd5162fa1f1758cfad277188b51a0c7
Joseph Snell <joepsnell@gmail.com>
2020-06-02 22:17:39 -0500

    update

---
+++ lib/generator.ts
@@ -0,0 +1,38 @@
+import * as ag from '@aws-cdk/aws-apigateway';
+import { Core } from './core';
+
+export enum ResourceTypes {
+  RESOURCE='RESOURCE',
+  METHOD='METHOD'
+}
+
+export interface ResourceMapItem {
+  path: string;
+  child?: ResourceMapItem;
+  isParameter: boolean;
+}
+
+export interface ResourceMap {
+  [key: string]: ResourceMapItem;
+}
+
+export class Generator {
+  private core: Core;
+  private resources: ResourceMap;
+
+  constructor(core: Core, resources: ResourceMap) {
+    this.core = core;
+    this.resources = resources;
+    
+    this.createResource(this.core.root, this.resources);
+  }
+  
+  private createResource(parent: ag.Resource | ag.IResource, next: ResourceMapItem) {
+    const current = parent.addResource(next.path);
+    if (next.child) {
+      return this.createResource(current, next.child);
+    } else {
+      return current
+    }
+  }
+}
---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+
+import { ResourceMap } from './generator';
+
+export const constructResourceMap = async (path: string): ResourceMap {
+  for await (const p of walk('/tmp/'))
+    console.log(p)
+};
+
+async function* walk(dir) {
+    for await (const d of await fs.promises.opendir(dir)) {
+        const entry = path.join(dir, d.name);
+        if (d.isDirectory()) yield* await walk(entry);
+        else if (d.isFile()) yield entry;
+    }
+}
