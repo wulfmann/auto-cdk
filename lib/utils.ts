ac5597ceafed70bd0248796ac6118659628a455e
Joseph Snell <joepsnell@gmail.com>
2020-06-03 18:20:43 -0500

    complete directory tree

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,27 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import { promises } as fs from 'fs';
+import { basename } from 'path';
+import { RouteType, RouteMap } from './routes';
 
-export async function* walk(dir: string): AsyncGenerator<string> {
-  for await (const d of await fs.promises.opendir(dir)) {
-    const entry = path.join(dir, d.name);
-    if (d.isDirectory()) yield* await walk(entry);
-    else if (d.isFile()) yield entry;
+export async function directoryTree(dir: string) {
+  const item = {
+    type: RouteType.DIRECTORY
   }
+
+  for await (const path of await fs.opendir(dir)) {
+    if (!('children' in item)) {
+      item.children = {};
+    }
+    const key = path.name;
+    if (path.isDirectory()) {
+      item.children[key] = await directoryTree(path);
+    } else if (path.isFile()) {
+      item.children[key] = {
+        type: RouteType.FILE
+      }
+    } else {
+      return null;
+    }
+  }
+
+  return item;
 }
